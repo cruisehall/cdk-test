1ec4ce5f7b7713813d9b1e3fd5f01c30e394fb81
Cruise Hall <19177287+cruisehall@users.noreply.github.com>
2025-02-02 20:14:52 +0000

    feat: Adds 'HelloCdkQueue'

--- hello-cdk/hello_cdk/hello_cdk_stack.py
+++ hello-cdk/hello_cdk/hello_cdk_stack.py
@@ -1,7 +1,7 @@
 from aws_cdk import (
-    # Duration,
+    Duration,
     Stack,
-    # aws_sqs as sqs,
+    aws_sqs as sqs,
 )
 from constructs import Construct
 
@@ -13,7 +13,7 @@ class HelloCdkStack(Stack):
         # The code that defines your stack goes here
 
         # example resource
-        # queue = sqs.Queue(
-        #     self, "HelloCdkQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+        queue = sqs.Queue(
+            self, "HelloCdkQueue",
+            visibility_timeout=Duration.seconds(300),
+        )
